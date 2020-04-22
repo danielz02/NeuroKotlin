e6a845d9694736afe66d282a0ea9faf79dc385d3
yihjian <yihongj2@illinois.edu>
2020-04-22 13:34:35 -0500

    Reformat code

--- src/main/kotlin/forward/Activation.kt
+++ src/main/kotlin/forward/Activation.kt
@@ -4,25 +4,30 @@ import org.jetbrains.numkt.core.*
 import org.jetbrains.numkt.math.*
 import org.jetbrains.numkt.zeros
 
-object Activation{
-    @ExperimentalNumkt
-    fun ReLU(x: KtNDArray<Float>): List<KtNDArray<Float>> {
-        val out = maximum(x, zeros(x.shape[0], x.shape[1]));
-        val cache = x;
-        return listOf(out, cache);
-    }
+/**
+ * Computes the forward pass for a layer of rectified linear units (ReLUs).
+ * @param x: Inputs, of any shape
+ * @return a list of(output, x) where output is the same shape as x, x as cache.
+ */
+@ExperimentalNumkt
+fun ReLU(x: KtNDArray<Float>): List<KtNDArray<Float>> {
+    val out = maximum(x, zeros(x.shape[0], x.shape[1]));
+    val cache = x;
+    return listOf(out, cache);
+}
 
-    @ExperimentalNumkt
-    fun Sigmoid(x: KtNDArray<Float>): List<KtNDArray<Float>> {
-        val out = Sigmoid(x) as KtNDArray<Float>;
-        val cache = x;
-        return listOf(out, cache);
-    }
+/** Computes the forward pass for a layer of Sigmoid, same signiture as ReLU */
+@ExperimentalNumkt
+fun Sigmoid(x: KtNDArray<Float>): List<KtNDArray<Float>> {
+    val out = 1 / 1 + exp(-x) as KtNDArray<Float>;
+    val cache = x;
+    return listOf(out, cache);
+}
 
-    @ExperimentalNumkt
-    fun TanH(x: KtNDArray<Float>): List<KtNDArray<Float>> {
-        val out = tanh(x) as KtNDArray<Float>;
-        val cache = x;
-        return listOf(out, cache);
-    }
+/** Computes the forward pass for a layer of tanh, same signiture as ReLU */
+@ExperimentalNumkt
+fun Tanh(x: KtNDArray<Float>): List<KtNDArray<Float>> {
+    val out = tanh(x) as KtNDArray<Float>;
+    val cache = x;
+    return listOf(out, cache);
 }
--- src/main/kotlin/forward/ForwardLayer.kt
+++ src/main/kotlin/forward/ForwardLayer.kt
@@ -1,20 +1,40 @@
 package forward
 
-import org.jetbrains.numkt.core.*
-import org.jetbrains.numkt.math.*
-import org.jetbrains.numkt.zeros
-import org.jetbrains.numkt.array
+import org.jetbrains.numkt.core.ExperimentalNumkt
+import org.jetbrains.numkt.core.KtNDArray
 
-import forward.Layer.Affine
-
-
-object ForwardLayer {
-    @ExperimentalNumkt
-    fun AffineReLUForward(x: KtNDArray<Float>, w: KtNDArray<Float>, b: KtNDArray<Float>): List<KtNDArray<Float>> {
-        val afCache = Affine(x,w,b);
-        val a = afCache[0];
-        val reLUCache = Activation.ReLU(a);
-        return listOf(reLUCache[0], afCache[1], afCache[2], afCache[3], reLUCache[1]);
-    }
-    //!TODO TanH and Sigmoid in same manner
+/**
+ * A convience layer that performs affine transform forward by a ReLU
+ * Wraps a affine layer and a ReLU layer
+ * @param x: input to affine layer
+ * @param w: weight to affine
+ * @param b: bias
+ * @return list in order(output, (affine caches), (ReLU cache))
+ */
+@ExperimentalNumkt
+fun AffineReLUForward(x: KtNDArray<Float>, w: KtNDArray<Float>, b: KtNDArray<Float>): List<KtNDArray<Float>> {
+    val afCache = Affine(x, w, b);
+    val a = afCache[0];
+    val reLUCache = ReLU(a);
+    return listOf(reLUCache[0], afCache[1], afCache[2], afCache[3], reLUCache[1]);
 }
+
+/** Convience layer for sigmoid forward */
+@ExperimentalNumkt
+fun AffineSigmoidForward(x: KtNDArray<Float>, w: KtNDArray<Float>, b: KtNDArray<Float>): List<KtNDArray<Float>> {
+    val afCache = Affine(x, w, b);
+    val a = afCache[0];
+    val sigCache = Sigmoid(a);
+    return listOf(sigCache[0], afCache[1], afCache[2], afCache[3], sigCache[1]);
+}
+
+/** Convience layer for tanh forward */
+@ExperimentalNumkt
+fun AffineTanhForward(x: KtNDArray<Float>, w: KtNDArray<Float>, b: KtNDArray<Float>): List<KtNDArray<Float>> {
+    val afCache = Affine(x, w, b);
+    val a = afCache[0];
+    val tanhCache = Tanh(a);
+    return listOf(tanhCache[0], afCache[1], afCache[2], afCache[3], tanhCache[1]);
+}
+
+//!TODO cov/bn/dropout
