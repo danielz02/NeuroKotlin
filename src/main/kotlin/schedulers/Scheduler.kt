8553ef212ac06e7383afa47445c538557107ceb9
danielz02 <chenhui5@illinois.edu>
2020-04-24 12:37:23 -0500

    Add Scheduler superclass

--- src/main/kotlin/schedulers/Scheduler.kt
+++ src/main/kotlin/schedulers/Scheduler.kt
@@ -2,20 +2,39 @@ package schedulers
 
 import kotlin.math.pow
 
+abstract class Scheduler(private val initialLr: Double = 0.01) {
+    abstract fun learningRate(step: Int): Double
+    abstract operator fun invoke(step: Int): Double
+}
+
+class ConstantScheduler(private val initialLr: Double = 0.01) : Scheduler(initialLr) {
+    override fun learningRate(step: Int): Double {
+        return this.initialLr
+    }
+
+    override fun toString(): String {
+        return "Constant Scheduler: Learning Rate ${this.initialLr}"
+    }
+
+    override operator fun invoke(step: Int): Double {
+        return learningRate(step)
+    }
+}
+
 class ExponentialScheduler(
-    private val initialLr: Number = 0.01,
+    private val initialLr: Double = 0.01,
     private val stageLength: Int = 500,
     private val stairCase: Boolean = false,
     private val decay: Double = 0.01
-) {
+) : Scheduler(initialLr) {
 
-    fun learningRate(step: Int): Double {
+    override fun learningRate(step: Int): Double {
         var currentStage = (step / this.stageLength.toDouble())
         return if (this.stairCase) {
             currentStage = kotlin.math.floor(currentStage)
-            this.initialLr.toDouble() * this.decay.pow(currentStage)
+            this.initialLr * this.decay.pow(currentStage)
         } else {
-            this.initialLr.toDouble() * this.decay.pow(currentStage)
+            this.initialLr * this.decay.pow(currentStage)
         }
     }
 
@@ -27,7 +46,7 @@ class ExponentialScheduler(
                 "Decay Factor: ${this.decay}"
     }
 
-    operator fun invoke(step: Int) {
-        this.learningRate(step)
+    override operator fun invoke(step: Int): Double {
+        return this.learningRate(step)
     }
 }
