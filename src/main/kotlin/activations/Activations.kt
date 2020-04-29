d75485deb8aa56e1ea44f7898ea3f1cb6ec91e41
danielz02 <chenhui5@illinois.edu>
2020-04-29 15:04:29 -0500

    Add activation base class

--- src/main/kotlin/activations/Activations.kt
+++ src/main/kotlin/activations/Activations.kt
@@ -1,1 +1,35 @@
 package activations
+
+import org.jetbrains.numkt.core.ExperimentalNumkt
+import org.jetbrains.numkt.core.KtNDArray
+import org.jetbrains.numkt.core.reshape
+import org.jetbrains.numkt.math.clip
+
+@ExperimentalNumkt
+abstract class Activations {
+    operator fun invoke(input: KtNDArray<Double>): KtNDArray<Double> {
+        return if (input.ndim == 1) {
+            input.reshape(1, -1)
+        } else {
+            fn(input)
+        }
+    }
+    abstract fun fn(input: KtNDArray<Double>): KtNDArray<Double>
+    abstract fun grad(input: KtNDArray<Double>): KtNDArray<Double>
+    abstract override fun toString(): String
+}
+
+@ExperimentalNumkt
+class ReLU : Activations() {
+    override fun fn(input: KtNDArray<Double>): KtNDArray<Double> {
+        return clip(input, 0.0, Double.POSITIVE_INFINITY)
+    }
+
+    override fun grad(input: KtNDArray<Double>): KtNDArray<Double> {
+        TODO("Not yet implemented")
+    }
+
+    override fun toString(): String {
+        TODO("Not yet implemented")
+    }
+}
