79d29e200151c46ef18d501ad60fe56955a4ee50
danielz02 <chenhui5@illinois.edu>
2020-05-01 08:21:20 -0500

    Refactor lr schedulers

--- src/main/kotlin/schedulers/Scheduler.kt
+++ src/main/kotlin/schedulers/Scheduler.kt
@@ -1,26 +1,29 @@
 package schedulers
 
+import org.jetbrains.numkt.core.ExperimentalNumkt
+import org.jetbrains.numkt.core.KtNDArray
 import kotlin.math.pow
 
+@ExperimentalNumkt
 abstract class Scheduler(private val initialLr: Double = 0.01) {
-    abstract fun learningRate(step: Int): Double
-    abstract operator fun invoke(step: Int): Double
+    abstract fun learningRate(step: Int, currentLoss: KtNDArray<Double>? = null): Double
+    operator fun invoke(step: Int, currentLoss: KtNDArray<Double>? = null): Double {
+        return this.learningRate(step, currentLoss)
+    }
 }
 
+@ExperimentalNumkt
 class ConstantScheduler(private val initialLr: Double = 0.01) : Scheduler(initialLr) {
-    override fun learningRate(step: Int): Double {
+    override fun learningRate(step: Int, currentLoss: KtNDArray<Double>?): Double {
         return this.initialLr
     }
 
     override fun toString(): String {
         return "Constant Scheduler: Learning Rate ${this.initialLr}"
     }
-
-    override operator fun invoke(step: Int): Double {
-        return learningRate(step)
-    }
 }
 
+@ExperimentalNumkt
 class ExponentialScheduler(
     private val initialLr: Double = 0.01,
     private val stageLength: Int = 500,
@@ -28,8 +31,8 @@ class ExponentialScheduler(
     private val decay: Double = 0.01
 ) : Scheduler(initialLr) {
 
-    override fun learningRate(step: Int): Double {
-        var currentStage = (step / this.stageLength.toDouble())
+    override fun learningRate(step: Int, currentLoss: KtNDArray<Double>?): Double {
+        var currentStage = step.toDouble() / this.stageLength.toDouble()
         return if (this.stairCase) {
             currentStage = kotlin.math.floor(currentStage)
             this.initialLr * this.decay.pow(currentStage)
@@ -44,9 +47,5 @@ class ExponentialScheduler(
                 "Stage Length ${this.stageLength}\n" +
                 "Stair Case Learning Rate: ${this.stairCase}\n" +
                 "Decay Factor: ${this.decay}"
-    }
-
-    override operator fun invoke(step: Int): Double {
-        return this.learningRate(step)
     }
 }
