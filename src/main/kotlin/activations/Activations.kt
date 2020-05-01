48d6bc445e3e2c3a35e63e835eb19531cb8c3202
danielz02 <chenhui5@illinois.edu>
2020-04-30 20:51:43 -0500

    Merge branch

--- src/main/kotlin/activations/Activations.kt
+++ src/main/kotlin/activations/Activations.kt
@@ -3,7 +3,12 @@ package activations
 import org.jetbrains.numkt.core.ExperimentalNumkt
 import org.jetbrains.numkt.core.KtNDArray
 import org.jetbrains.numkt.core.reshape
-import org.jetbrains.numkt.math.clip
+import org.jetbrains.numkt.math.exp
+import org.jetbrains.numkt.math.maximum
+import org.jetbrains.numkt.math.plus
+import org.jetbrains.numkt.math.tanh
+import org.jetbrains.numkt.math.unaryMinus
+import org.jetbrains.numkt.zeros
 
 @ExperimentalNumkt
 abstract class Activations {
@@ -19,17 +24,30 @@ abstract class Activations {
     abstract override fun toString(): String
 }
 
+/**
+ * Computes the forward pass for a layer of rectified linear units (ReLUs).
+ * @param x: Inputs, of any shape
+ * @return a list of(output, x) where output is the same shape as x, x as cache.
+ */
 @ExperimentalNumkt
-class ReLU : Activations() {
-    override fun fn(input: KtNDArray<Double>): KtNDArray<Double> {
-        return clip(input, 0.0, Double.POSITIVE_INFINITY)
-    }
+fun ReLU(x: KtNDArray<Float>): List<KtNDArray<Float>> {
+    val out = maximum(x, zeros(x.shape[0], x.shape[1]))
+    val cache = x
+    return listOf(out, cache)
+}
 
-    override fun grad(input: KtNDArray<Double>): KtNDArray<Double> {
-        TODO("Not yet implemented")
-    }
+/** Computes the forward pass for a layer of Sigmoid, same signiture as ReLU */
+@ExperimentalNumkt
+fun Sigmoid(x: KtNDArray<Float>): List<KtNDArray<Float>> {
+    val out = 1 / 1 + exp(-x) as KtNDArray<Float>
+    val cache = x
+    return listOf(out, cache)
+}
 
-    override fun toString(): String {
-        TODO("Not yet implemented")
-    }
+/** Computes the forward pass for a layer of tanh, same signiture as ReLU */
+@ExperimentalNumkt
+fun Tanh(x: KtNDArray<Float>): List<KtNDArray<Float>> {
+    val out = tanh(x) as KtNDArray<Float>
+    val cache = x
+    return listOf(out, cache)
 }
